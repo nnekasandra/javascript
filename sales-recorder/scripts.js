b23a2b16cf99ed6fd652821967038722fc4fdcc3
nnekasandra <nnekasandra2016@gmail.com>
2020-11-15 18:25:43 +0100

    updated styles for calculator-app and improved on my first draft for sales-recorder

--- sales-recorder/scripts.js
+++ sales-recorder/scripts.js
@@ -5,11 +5,48 @@ function addRow() {
     var rowCount=table.rows.length;
     var addRow = table.insertRow(rowCount);
     var cell1=addRow.insertCell(0);
-    cell1.innerHTML=rowCount + 1;
+    cell1.innerHTML= rowCount++;
     var cell2 = addRow.insertCell(1);
     var element=document.createElement("input");
     element.type="text";
-    element.name="taxtbox";
+    element.name = 'item-name';
+    var cell3 = addRow.insertCell(2);
+    var element2 = document.createElement('input');
+    element2.type = 'number';
+    element2.min = 10;
+    element2.name = 'cost-price';
+    var cell4 = addRow.insertCell(3)
+    var element3 = document.createElement('input');
+    element3.type = 'number';
+    element3.name = 'selling-price';
+    element3.min = 10;
     cell2.appendChild(element);
+    cell3.appendChild(element2)
+    cell4.appendChild(element3)
     }
     
+
+
+
+
+
+
+
+
+
+    /*function addRow() {
+        var table=document.getElementById("addTable");
+        var rowCount=table.rows.length;
+        var addRow = table.insertRow(rowCount);
+        var cell1=addRow.insertCell(0);
+        cell1.innerHTML=rowCount + 1;
+        var cell2 = addRow.insertCell(1);
+        var element=document.createElement("input");
+        element.type="text";
+        element.name="taxtbox";
+        var cell3 = addRow.insertCell(2);
+        var element2 = document.createElement('input');
+        element.type = 'number';
+        element.name = 'item-price'
+        cell2.appendChild(element);
+        }*/
